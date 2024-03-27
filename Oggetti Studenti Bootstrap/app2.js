c3ab4434a1a20b0caf5485c7d332c5474fbcf21c
RFlow3r7 <andrefiore19967@gmail.com>
2024-03-27 20:30:37 +0100

    Edit: Completato

--- Oggetti Studenti Bootstrap/app2.js
+++ Oggetti Studenti Bootstrap/app2.js
@@ -1,58 +1,42 @@
 console.log('Esercizio JS Studenti');
 
-// Milestone 1 //
-//Creazione Array Studenti//
+// Creazione Array Studenti
+let studenti = [];
 
-let studenti = [
-    {
-        Nome: 'Andrea',
-        Cognome: 'Fiore',
-        Eta: 28
-    },
+// Aggiunta nuovo studente nell'array
+function aggiungiStudente(name, lastname, age) {
+    const NuovoStudente = {
+        name: name,
+        lastname: lastname,
+        age: age
+    };
+    studenti.push(NuovoStudente);
+    aggiornaInterfaccia();
+}
 
-    {
-        Nome: 'Luigi',
-        Cognome: 'Rossi',
-        Eta: 30
-    },
+// Funzione per aggiornare l'interfaccia HTML
+function aggiornaInterfaccia() {
+    const studentlist = document.getElementById('student-list');
+    studentlist.innerHTML = '';
+    studenti.forEach(studente => {
+        const row = `
+            <tr>
+                <td>${studente.name}</td>
+                <td>${studente.lastname}</td>
+                <td>${studente.age}</td>
+            </tr>
+        `;
+        studentlist.innerHTML += row;
+    });
+}
 
-    {
-        Nome: 'Marco',
-        Cognome: 'Fizzo',
-        Eta: 32
-    },
-]
+// Invio del modulo per aggiungere uno studente
 
-// Milestone 2 //
-// Chiedi all'utente di inserire nome, cognome ed età del nuovo studente
-let nuovoNome = prompt("Inserisci nome del nuovo studente: ");
-let nuovoCognome = prompt("Inserisci cognome del nuovo studente: ");
-let nuovoEta = parseInt(prompt("inserisci l'età del nuovo studente: "));
-
-// Creazione nuovo oggetto studente in base a cosa inserisce l'utente //
-
-let nuovoStudente =
-{
-    Nome: nuovoNome,
-    Cognome: nuovoCognome,
-    Eta: nuovoEta
-};
-
-
-// Aggiunta studente nell'array già esistente //
-studenti.push(nuovoStudente);
-
-//Ciclo For per ciclare//
-
-// for (let i = 0; i < studenti.length; i++) {
-// console.log(`Nome: ${studenti[i].Nome}`);
-// console.log(`Cognome: ${studenti[i].Cognome}`);
-// console.log(`Età: ${studenti[i].Eta}`);
-// }
-
-
-studenti.forEach(studente => {
-    console.log(`Nome: ${studente.Nome}`);
-    console.log(`Cognome: ${studente.Cognome}`);
-    console.log(`Età: ${studente.Eta}`);
+document.getElementById('form-add-members').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const lastname = document.getElementById('last_name').value
+    const age = document.getElementById('age').value
+    aggiungiStudente(name, lastname, age);
+    this.reset()
 });
